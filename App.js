505ec69be2d2f544623f12aa704b1a17a1da4dae
EUGENIO NEVES SPERA <eugenio.nspera@senacsp.edu.br>
2024-09-16 17:19:46 -0300

    Iniciado exercicio meu primeiro App prof Eduardo

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import react,{Component} from 'react';
+import {View, Text, Image} from 'react-native';
+
+class App extends Component{
+  render (){
+    return(
+
+      <View>
+        <Text> Olá mundo</Text>
+        <Text style={{
+
+          color:'red',//cor
+          fontSize:25,//tamanho da fonte
+          margin:15,
+          fontWeight:'bold',//negrito
+          fontStyle:'italic',//italico
+          fontFamily:'Arial',//tipo de fonte
+          textAlign:'center',//centralizar
+          lineHeight:35,//define a altura
+          letterSpacing:2, //espaçamento entre as letras 
+          textDecorationLine:'underline',//sublinhado
+          textDecorationStyle:'dotted',//estilo do sublinhado
+          textTransform:'uppercase',//letra maíscula
+
+        }}>Aula de mobile</Text>
+        <Image source={{uri:'https://sujeitoprogramador.com/steve.png'}}
+        style={{width:300, height:300, marginBottom:20}}></Image>
+
+<Image source={require('./assets/Bill_Gates.jpg')}
+        style={{width:300, height:300, marginBottom:20}}></Image>
+
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
